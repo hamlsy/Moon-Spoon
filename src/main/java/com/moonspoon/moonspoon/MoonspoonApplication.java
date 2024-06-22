a97bfda8fecefc58b40fb7c3e02cbc9d33367544
hamsterdmin <dltmddud1122@naver.com>
2024-06-22 20:40:49 +0900

    init: add project

---
+++ src/main/java/com/moonspoon/moonspoon/MoonspoonApplication.java
@@ -0,0 +1,13 @@
+package com.moonspoon.moonspoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonspoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonspoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonspoon/moonspoon/MoonspoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonspoon.moonspoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonspoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
