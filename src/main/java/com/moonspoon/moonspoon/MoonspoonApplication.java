b47b4590bcbd29c0a941faea792364f46ff14db9
hamsterdmin <dltmddud1122@naver.com>
2024-06-24 00:16:50 +0900

    #3 feat: 로그인, 회원가입 폼 구현

--- src/main/java/com/moonspoon/moonspoon/MoonspoonApplication.java
+++ src/main/java/com/moonspoon/moonspoon/MoonspoonApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//시큐리티 일시중지
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoonspoonApplication {
 
 	public static void main(String[] args) {
