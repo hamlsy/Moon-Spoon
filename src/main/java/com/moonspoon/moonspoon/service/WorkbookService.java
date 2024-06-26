699953b675725e4df044d259deb3e319207d4415
hamsterdmin <dltmddud1122@naver.com>
2024-06-26 18:16:10 +0900

    #17 feat: Transactional 적용

--- src/main/java/com/moonspoon/moonspoon/service/WorkbookService.java
+++ src/main/java/com/moonspoon/moonspoon/service/WorkbookService.java
@@ -2,17 +2,21 @@ package com.moonspoon.moonspoon.service;
 
 import com.moonspoon.moonspoon.domain.Workbook;
 import com.moonspoon.moonspoon.repository.WorkbookRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class WorkbookService {
     private final WorkbookRepository workbookRepository;
 
+    @Transactional
     public Workbook createWorkbook(Workbook workbook){
         return workbookRepository.save(workbook);
     }
@@ -25,12 +29,15 @@ public class WorkbookService {
         return workbookRepository.findAll();
     }
 
+    @Transactional
     public Workbook updateWorkbook(Long id, Workbook updateWorkbook){
         Workbook workbook = workbookRepository.findById(id).orElseThrow();
         LocalDateTime currentTime = LocalDateTime.now();
         workbook.update(updateWorkbook.getTitle(), updateWorkbook.getContent(), currentTime);
         return workbook;
     }
+
+    @Transactional
     public void deleteWorkbook(Long id){
         workbookRepository.deleteById(id);
     }
