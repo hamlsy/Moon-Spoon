301363e801a174488a79dd7c4409310b2354c22b
hamlsy <dltmddud1122@naver.com>
2024-06-27 16:29:53 +0900

    #19 feat: JpaRepository @repository 어노테이션 제거

--- src/main/java/com/moonspoon/moonspoon/repository/UserRepository.java
+++ src/main/java/com/moonspoon/moonspoon/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.moonspoon.moonspoon.repository;
 
 import com.moonspoon.moonspoon.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository {
+
+public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.username = :username")
     User findByUsername(String username);
 }
