ba4bf6eda5a72878ea7a8092ca363877fe347409
hamsterdmin <dltmddud1122@naver.com>
2024-06-29 14:41:02 +0900

    feat: test request 구현 (#27)

---
+++ src/main/java/com/moonspoon/moonspoon/dto/request/test/TestRequest.java
@@ -0,0 +1,13 @@
+package com.moonspoon.moonspoon.dto.request.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestRequest {
+    private boolean isRandom;
+    private int problemCount;
+    private String sortOrder;
+
+}
