1823ffb76b96c76ac58ae6856e3d42f6f4eca20b
hamsterdmin <dltmddud1122@naver.com>
2024-07-05 16:03:36 +0900

    feat: 아이디 ,이름(닉네임) 제한조건 구현 (#43)

--- src/main/java/com/moonspoon/moonspoon/dto/request/user/CheckNameRequest.java
+++ src/main/java/com/moonspoon/moonspoon/dto/request/user/CheckNameRequest.java
@@ -1,6 +1,8 @@
 package com.moonspoon.moonspoon.dto.request.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +10,7 @@ import lombok.Setter;
 @Setter
 public class CheckNameRequest {
     @NotBlank(message = "이름(닉네임)을 입력하세요.")
+    @Size(min = 4, max = 16)
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s_-]+$", message = "특수문자는 허용되지 않습니다.")
     private String name;
 }
--- src/main/java/com/moonspoon/moonspoon/dto/request/user/UserSignupRequest.java
+++ src/main/java/com/moonspoon/moonspoon/dto/request/user/UserSignupRequest.java
@@ -3,6 +3,8 @@ package com.moonspoon.moonspoon.dto.request.user;
 import com.moonspoon.moonspoon.domain.User;
 import com.moonspoon.moonspoon.domain.UserRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,9 +13,13 @@ import lombok.Setter;
 public class UserSignupRequest {
 
     @NotBlank(message = "아이디를 입력하세요.")
+    @Size(min = 4, max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문과 숫자로 이루어져야 합니다.")
     private String username;
 
     @NotBlank(message = "이름을 입력하세요.")
+    @Size(min = 4, max = 16)
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s_-]+$", message = "특수문자는 허용되지 않습니다.")
     private String name;
 
     @NotBlank(message = "비밀번호를 입력하세요.")
