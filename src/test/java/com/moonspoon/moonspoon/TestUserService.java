6b59b3211f17a79191bab3292a1c1f729955a67e
hamsterdmin <dltmddud1122@naver.com>
2024-07-06 17:10:31 +0900

    feat: repository, service 코드 중복 체크 추가 (#45)

--- src/test/java/com/moonspoon/moonspoon/TestUserService.java
+++ src/test/java/com/moonspoon/moonspoon/TestUserService.java
@@ -1,5 +1,6 @@
 package com.moonspoon.moonspoon;
 
+import com.moonspoon.moonspoon.exception.DuplicateUserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,11 +8,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class TestUserService {
     @Autowired
-    private TestUserRepository testUserRepository;
+    private TestUserRepository repository;
 
     @Transactional
-    public TestUser signup(TestUser testUser){
-        return testUserRepository.save(testUser);
+    public void signup(TestUser testUser){
+        if(repository.existsByName(testUser.getName())){
+            throw new DuplicateUserException("존재하는 이름입니다.");
+        }
+        repository.save(testUser);
     }
 
 
--- src/test/java/com/moonspoon/moonspoon/user/UserServiceTest.java
+++ src/test/java/com/moonspoon/moonspoon/user/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.moonspoon.moonspoon.user;
 
+import com.moonspoon.moonspoon.TestUser;
 import com.moonspoon.moonspoon.TestUserRepository;
 import com.moonspoon.moonspoon.TestUserService;
 import com.moonspoon.moonspoon.repository.UserRepository;
@@ -8,6 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,9 +26,31 @@ public class UserServiceTest {
     private TestUserRepository testUserRepository;
 
     @Test
-    void concurrentSignupTest(){
+    void concurrentSignupTest() throws InterruptedException{
+        int threadCount = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
 
+        for (int i = 0; i < threadCount; i++) {
+            final int index = i;
+            executorService.submit(() -> {
+                try {
+                    TestUser user = new TestUser();
+                    user.setName("name" + index);
+                    // 회원가입 시도
+                    testUserService.signup(user);
+                } catch (Exception e) {
+                    // 예외 처리
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
 
+        latch.await();
+
+        // 모든 쓰레드가 실행을 완료한 후, 회원 수 검증
+        assertEquals(threadCount, testUserRepository.count());
     }
-
 }
