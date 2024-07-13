2453d344383b7cf3b30178a53b4bd2ddcc2fa23e
hamlsy <dltmddud1122@naver.com>
2024-07-13 16:26:19 +0900

    feat: docker-compose 내용 축 (#49)

--- vue-cli/vue.config.js
+++ vue-cli/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // build 경로 설정
-  outputDir: "../src/main/resources/static",
+  outputDir: "dist",
   devServer: {
     // 프록시 설정
     proxy: 'http://localhost:8080'
