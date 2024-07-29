0e8a543d15fa395b6dac4db54bebb7acfabedca0
hamsterdmin <dltmddud1122@naver.com>
2024-07-29 12:55:32 +0900

    add .idea file

--- backend/src/main/java/com/moonspoon/moonspoon/dto/request/user/UserSignupRequest.java
+++ backend/src/main/java/com/moonspoon/moonspoon/dto/request/user/UserSignupRequest.java
@@ -18,7 +18,7 @@ public class UserSignupRequest {
     private String username;
 
     @NotBlank(message = "이름을 입력하세요.")
-    @Size(min = 4, max = 16)
+    @Size(min = 2, max = 16)
     @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s_-]+$", message = "특수문자는 허용되지 않습니다.")
     private String name;
 
